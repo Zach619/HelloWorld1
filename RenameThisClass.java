1cfcc45129acad86ede779de2aec9eb08f1c65fb
Zach619 <112730663+Zach619@users.noreply.github.com>
2022-09-26 14:18:57 -0700

    Update RenameThisClass.java

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -5,16 +5,18 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RenameThisClass
+public class Arrays
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
-        for (int i = 1; < strArray.length, i++) {
+        for (int i = 1; i < strArray.length; i++) {
             strArray[i] = strArray[i] + myArray[i-1];
         }
         for (int value : strArray) {
-            System.out.println(value * 2);
+            System.out.println(i);
         }
+        for (int value : strArray) {
+            System.out.println
     }
 }
