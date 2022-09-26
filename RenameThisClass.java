6add47e04d8304adefa2ee3c2cbcb24084644d66
Zachary Wenger <zachary.wenger@mrjaffesclass.com>
2022-09-26 14:12:22 -0700

    Commiting Arrays

---
+++ RenameThisClass.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RenameThisClass
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
+        for (int i = 1; < strArray.length, i++) {
+            strArray[i] = strArray[i] + myArray[i-1];
+        }
+        for (int value : strArray) {
+            System.out.println(value * 2);
+        }
+    }
+}
